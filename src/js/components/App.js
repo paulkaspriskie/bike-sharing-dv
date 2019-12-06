b27007030828a49006a25c448b361582cb147242
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-06 12:37:12 -0500

    Updated App.js: Added api fetch to componentDidMount

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,6 +3,17 @@ import React from 'react';
 
 class App extends React.Component {
 
+  componentDidMount() {
+    const url = "http://localhost:3000/data"
+
+    fetch(url, { method: 'get' })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      }).catch((error) => console.log(error));
+  }
+
+
   render() {
     return (
       <div>
