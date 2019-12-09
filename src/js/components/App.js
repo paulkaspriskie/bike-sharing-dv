6115f4b97807ef417f1d0b76f7bc4ef7e7f2935b
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-09 13:15:08 -0500

    Created function getOccurrences to count value occurrences

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -29,10 +29,19 @@ class App extends React.Component {
       Object.values(this.state.data).map((items, i) => {
         // console.log(items.start_station);
         // console.log(items.passholder_type);
+        // console.log(items.bike_type);
         durationArr.push(Number(items.duration));
       });
     });
 
+    function getOccurrences(array, value) {
+      var count = 0;
+      array.forEach((v) => (v === value && count++));
+
+      return count;
+    }
+
+    console.log(getOccurrences(durationArr, 3));
     var getAvgDur = Math.round(durationArr.reduce((a,b) => a + b, 0) / durationArr.length);
   }
 
