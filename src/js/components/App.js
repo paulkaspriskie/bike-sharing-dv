8b380b1ce82f46aea1c102855d9dfd123e43979d
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-09 12:45:07 -0500

    Updated App.js: created initalMap promise

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -17,22 +17,23 @@ class App extends React.Component {
       .then(response => response.json())
       .then(data => {
         this.setState({ data: data });
-        this.dataDestructure()
+        this.dataDestructure();
       }).catch((error) => console.log(error));
   }
 
 
   dataDestructure() {
     var durationArr = [];
-    // var getAvgDur = durationArr.reduce((a,b) => a + b, 0) / durationArr.length;
 
-    Object.values(this.state.data).map((items, i) => {
-      // console.log(items.start_station);
-      // console.log(items.passholder_type);
-      durationArr.push(Number(items.duration));
+    const initialMap = new Promise(() => {
+      Object.values(this.state.data).map((items, i) => {
+        // console.log(items.start_station);
+        // console.log(items.passholder_type);
+        durationArr.push(Number(items.duration));
+      });
     });
 
-    console.log(durationArr);
+    var getAvgDur = Math.round(durationArr.reduce((a,b) => a + b, 0) / durationArr.length);
   }
 
 
