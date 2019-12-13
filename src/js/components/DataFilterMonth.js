8c1d4f3940f3d42595e327f2fa4f6871c75db7e1
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-13 12:44:46 -0500

    Created function: filterMonth

--- src/js/components/DataFilterMonth.js
+++ src/js/components/DataFilterMonth.js
@@ -4,10 +4,17 @@ import React from 'react';
 class DataFilterMonth extends React.Component {
 
   componentDidMount() {
-    const julyData = Object.values(this.props.data).filter((getMonth) => {
-      var date = new Date(getMonth.start_time);
-      return Number(date.getMonth()) === 6;
-    });
+
+    function filterMonth(data, value) {
+      return Object.values(data).filter((getMonth) => {
+        var date = new Date(getMonth.start_time);
+        return Number(date.getMonth()) === value;
+      });
+    }
+
+    var dataJuly = filterMonth(this.props.data, 6);
+    var dataAug = filterMonth(this.props.data, 7);
+    var dataSept = filterMonth(this.props.data, 8);
   }
 
   render() {
