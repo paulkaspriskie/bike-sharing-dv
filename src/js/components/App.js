afe06fc4334f47533db93addc77eab212252c7f5
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-19 12:12:48 -0500

    refactored data-flow: made DoughnutChart child of DataDashboard

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import PassTypeDoughnut from './charts/PassTypeDoughnut';
-import DataDashboard from './DataDashboard';
 import { Doughnut } from 'react-chartjs-2';
+import DataDashboard from './DataDashboard';
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      dataFetch: {},
-      dataChart: {}
-    };
-
-    this.dataDestructure = this.dataDestructure.bind(this);
+    this.state = { dataFetch: {} };
   }
 
 
@@ -23,66 +18,15 @@ class App extends React.Component {
       .then(response => response.json())
       .then(data => {
         this.setState({ dataFetch: data });
-        this.dataDestructure();
+
       }).catch((error) => console.log(error));
   }
 
 
-  dataDestructure() {
-    var tripDateArr = [],
-        durationArr = [],
-        bikeTypeArr = [],
-        passTypeArr = [],
-        tripTypeArr = [];
-
-    const initialMap = new Promise(() => {
-      Object.values(this.state.dataFetch).map((items, i) => {
-        var date = new Date(items.start_time);
-
-        tripDateArr.push(date.toISOString().substring(0, 10));
-        durationArr.push(Number(items.duration));
-        bikeTypeArr.push(items.bike_type);
-        passTypeArr.push(items.passholder_type);
-        tripTypeArr.push(items.trip_route_category);
-      });
-    });
-
-    function getOccurrences(array, value) {
-      var count = 0;
-      array.forEach((v) => (v === value && count++));
-
-      return count;
-    }
-
-    var bikeTypeStandard = getOccurrences(bikeTypeArr, 'standard'),
-        bikeTypeEletric = getOccurrences(bikeTypeArr, 'electric');
-
-    var tripTypeOneWay = getOccurrences(tripTypeArr, 'One Way'),
-        tripTypeRound = getOccurrences(tripTypeArr, 'Round Trip');
-
-    var passTypeDay = getOccurrences(passTypeArr, 'Day Pass'),
-        passTypeMonth = getOccurrences(passTypeArr, 'Indego30'),
-        passTypeYear = getOccurrences(passTypeArr, 'Indego365');
-
-    var getAvgDur = Math.round(durationArr.reduce((a,b) => a + b, 0) / durationArr.length);
-
-    this.setState({
-      dataChart: {
-        tripDate: tripDateArr,
-        avgTripTime: getAvgDur,
-        bikeType: { standard: bikeTypeStandard, electric: bikeTypeEletric },
-        passType: { dayPass: passTypeDay, monthPass: passTypeMonth, yearPass: passTypeYear },
-        tripType: { oneWay: tripTypeOneWay, roundTrip: tripTypeRound }
-      }
-    });
-  }
-
   render() {
     return (
       <div className="indego--container--app--layout">
-        { Object.keys(this.state.dataChart).length !== 0 ?
-          <PassTypeDoughnut passTypeData={this.state.dataChart.passType} /> : null }
-        { Object.keys(this.state.dataChart).length !== 0 ?
+        { Object.keys(this.state.dataFetch).length !== 0 ?
           <DataDashboard data={this.state.dataFetch} /> : null }
       </div>
     )
--- src/js/components/DataDashboard.js
+++ src/js/components/DataDashboard.js
@@ -1,27 +1,84 @@
 import React from 'react';
+import PassTypeDoughnut from './charts/PassTypeDoughnut';
 import BikeTypeBar from './charts/BikeTypeBar';
 
 
 class DataDashboard extends React.Component {
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = { dataChart: {} };
 
-    function filterMonth(data, value) {
-      return Object.values(data).filter((getMonth) => {
-        var date = new Date(getMonth.start_time);
-        return Number(date.getMonth()) === value;
+    this.dataDestructure = this.dataDestructure.bind(this);
+  }
+
+  componentDidMount() {
+    this.dataDestructure();
+  }
+
+  dataDestructure() {
+    var tripDateArr = [],
+        durationArr = [],
+        bikeTypeArr = [],
+        passTypeArr = [],
+        tripTypeArr = [];
+
+    const initialMap = new Promise(() => {
+      Object.values(this.props.data).map((items, i) => {
+        var date = new Date(items.start_time);
+
+        tripDateArr.push(date.toISOString().substring(0, 10));
+        durationArr.push(Number(items.duration));
+        bikeTypeArr.push(items.bike_type);
+        passTypeArr.push(items.passholder_type);
+        tripTypeArr.push(items.trip_route_category);
       });
+    });
+
+    function getOccurrences(array, value) {
+      var count = 0;
+      array.forEach((v) => (v === value && count++));
+
+      return count;
     }
 
-    // var dataJuly = filterMonth(this.props.data, 6);
-    // var dataAug = filterMonth(this.props.data, 7);
-    // var dataSept = filterMonth(this.props.data, 8);
+    var bikeTypeStandard = getOccurrences(bikeTypeArr, 'standard'),
+        bikeTypeEletric = getOccurrences(bikeTypeArr, 'electric');
+
+    var tripTypeOneWay = getOccurrences(tripTypeArr, 'One Way'),
+        tripTypeRound = getOccurrences(tripTypeArr, 'Round Trip');
+
+    var passTypeDay = getOccurrences(passTypeArr, 'Day Pass'),
+        passTypeMonth = getOccurrences(passTypeArr, 'Indego30'),
+        passTypeYear = getOccurrences(passTypeArr, 'Indego365');
+
+    var getAvgDur = Math.round(durationArr.reduce((a,b) => a + b, 0) / durationArr.length);
+
+    this.setState({
+      dataChart: {
+        tripDate: tripDateArr,
+        avgTripTime: getAvgDur,
+        bikeType: { standard: bikeTypeStandard, electric: bikeTypeEletric },
+        passType: { dayPass: passTypeDay, monthPass: passTypeMonth, yearPass: passTypeYear },
+        tripType: { oneWay: tripTypeOneWay, roundTrip: tripTypeRound }
+      }
+    });
+
+    // month filter
+    // function filterMonth(data, value) {
+    //   return Object.values(data).filter((getMonth) => {
+    //     var date = new Date(getMonth.start_time);
+    //     return Number(date.getMonth()) === value;
+    //   });
+    // }
+
   }
 
   render() {
-
     return(
-      <div className="indego--component--chart--bar--horizontal">
+      <div className="indego--component--chart">
+        { Object.keys(this.state.dataChart).length !== 0 ?
+            <PassTypeDoughnut passTypeData={this.state.dataChart.passType} /> : null }
         <BikeTypeBar />
       </div>
     )
