26186b82f507dae8f7cf56d775698a6263f352c5
Paul Kaspriskie <tug36513@stmc-l05phtdg.tu.temple.edu>
2019-12-19 08:50:07 -0500

    Added sample bar chart

--- src/js/components/DataFilterMonth.js
+++ src/js/components/DataFilterMonth.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { HorizontalBar } from 'react-chartjs-2';
 
 class DataFilterMonth extends React.Component {
 
@@ -12,14 +12,49 @@ class DataFilterMonth extends React.Component {
       });
     }
 
-    var dataJuly = filterMonth(this.props.data, 6);
-    var dataAug = filterMonth(this.props.data, 7);
-    var dataSept = filterMonth(this.props.data, 8);
+    // var dataJuly = filterMonth(this.props.data, 6);
+    // var dataAug = filterMonth(this.props.data, 7);
+    // var dataSept = filterMonth(this.props.data, 8);
   }
 
   render() {
+
+    const data = {
+      labels: [ 'July', 'August', 'September'],
+      datasets: [
+        {
+          label: 'test',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [65, 59, 80 ]
+        }
+      ]
+    }
     return(
-      <div> </div>
+      <div>
+        <HorizontalBar
+          data={data}
+          options={{
+            maintainAspectRatio: true,
+            title:{
+              display: true,
+              text:'Indego Passholder Plans',
+              fontSize: 20,
+              padding: 20
+            },
+            legend:{
+              display: true,
+              position:'bottom',
+              labels: {
+                padding: 25,
+                fontSize: 16
+              }
+            }
+          }} />
+      </div>
     )
   }
 
