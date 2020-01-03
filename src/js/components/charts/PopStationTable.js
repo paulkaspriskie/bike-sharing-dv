7da3e6d1492615f698e0c3984af5fc1c92c35494
Paul Kaspriskie <paulkaspriskie@gmail.com>
2020-01-03 14:03:56 -0500

    PopStationTable: added render condition & popStatArr state

--- src/js/components/charts/PopStationTable.js
+++ src/js/components/charts/PopStationTable.js
@@ -5,7 +5,11 @@ class PopStationTable extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { stationData: [] };
+    this.state = {
+      stationData: {},
+      popStatArr: []
+    };
+
     this.getStationOccur = this.getStationOccur.bind(this);
   }
 
@@ -34,11 +38,17 @@ class PopStationTable extends React.Component {
 
     let sortStats = Object.keys(counts).sort((a, b) => counts[b] - counts[a]);
     let getPopStats = sortStats.slice(0, 5);
+
+    this.setState({ popStatArr: getPopStats });
   }
 
   render() {
     return(
-      <div className="indego--component--table"></div>
+      <div className="indego--component--table">
+        { Object.keys(this.props.qData).length > 0 ? <ul>
+
+        </ul> : null }
+      </div>
     )
   }
 }
